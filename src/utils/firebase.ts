5f0eb3453e957bf53c94b61cf670f14d893b656f
AnnatarHe <iamhele1994@gmail.com>
2019-08-10 01:02:32 +0800

    fix(firebase): fix production build

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,11 +1,13 @@
-import * as firebase from 'firebase'
-
-firebase.initializeApp({
-  apiKey: "AIzaSyBJGBsMBEo59p-dMphRUtYgst8NZyZuy5Q",
-  authDomain: "clippingkk-mobile.firebaseapp.com",
-  databaseURL: "https://clippingkk-mobile.firebaseio.com",
-  projectId: "clippingkk-mobile",
-  storageBucket: "clippingkk-mobile.appspot.com",
-  messagingSenderId: "882301341703",
-  appId: "1:882301341703:web:b0f8f40b8b538d75"
-})
+// import * as firebase from 'firebase'
+import firebase from  'firebase/app'
+if (process.env.NODE_ENV === 'production') {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBJGBsMBEo59p-dMphRUtYgst8NZyZuy5Q",
+    authDomain: "clippingkk-mobile.firebaseapp.com",
+    databaseURL: "https://clippingkk-mobile.firebaseio.com",
+    projectId: "clippingkk-mobile",
+    storageBucket: "clippingkk-mobile.appspot.com",
+    messagingSenderId: "882301341703",
+    appId: "1:882301341703:web:b0f8f40b8b538d75"
+  })
+}
