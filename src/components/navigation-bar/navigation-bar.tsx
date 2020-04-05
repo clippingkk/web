1111552954662fcd5bf702ec6eb6b2f04a96e66e
AnnatarHe <iamhele1994@gmail.com>
2020-04-05 14:44:52 +0800

    fix(fonts): fix google fonts proxy issue

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -56,7 +56,7 @@ class NavigationBar extends React.PureComponent<any, any> {
       <nav className={styles.navbar}>
         <div className={styles.menu}>
           <img
-            src={require('../../assets/logo.png')}
+            src={require('../../assets/logo.png').default}
             alt="clippingkk logo"
             className={styles.logo}
           />
