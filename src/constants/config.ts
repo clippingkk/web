e9fd10211d6ff145619a12f94a845586454e53c9
AnnatarHe <iamhele1994@gmail.com>
2020-08-03 22:17:54 +0800

    feat(config): move api to another api

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 export const API_HOST = process.env.NODE_ENV === 'production' ?
-  'https://api.clippingkk.annatarhe.com' :
+  'https://clippingkk-api.annatarhe.com' :
   'http://localhost:9654'
 export const GithubClientID = '3659f6b0ecfe917d69e6'
