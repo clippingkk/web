eca1520f04b0558409ed3fad98e5c91c2904ee4f
AnnatarHe <iamhele1994@gmail.com>
2020-11-11 23:55:23 +0800

    feat(profile): add profile analysis data

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import {
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,
+} from 'recharts'
 import { getUserProfile, IUserProfile } from '../../services/auth';
 import Card from '../../components/card/card';
 import Divider from '../../components/divider/divider';
@@ -9,6 +12,7 @@ import profileQuery from '../../schema/profile.graphql'
 import { profile, profileVariables } from '../../schema/__generated__/profile';
 import WechatBindButton from './bind';
 import { useTranslation } from 'react-i18next';
+import dayjs from 'dayjs';
 
 const styles = require('./profile.css').default
 
@@ -29,17 +33,49 @@ function Profile(props: TProfileProps) {
   useTitle(data?.me.name)
   const { t } = useTranslation()
 
+  const chartData = useMemo(() => {
+    const c = []
+    for (let i = 12; i > 0; i--) {
+      const date = dayjs().startOf('month').subtract(i, 'month').format('YYYY-MM')
+      c.push({
+        date,
+        count: data?.me.analysis.monthly.find(x => x.date === date)?.count ?? 0
+      })
+    }
+    return c
+
+  }, [data?.me.analysis.monthly.length])
+
+
   return (
     <section>
-      <Card className='flex items-center justify-center py-12 w-full lg:w-4/5 mx-auto my-20 anna-fade-in'>
-        <img src={data?.me.avatar} className={styles.avatar} />
-        <div className={styles.info}>
-          <h3 className='text-2xl'>{data?.me.name}</h3>
-          <h5 className='text-lg text-gray-800'>{t('app.profile.collected')} {data?.me.clippingsCount} {t('app.profile.records')}</h5>
-          {!data?.me.wechatOpenid && (
-            <WechatBindButton />
-          )}
+      <Card className='flex items-center justify-center py-12 w-full lg:w-4/5 mx-auto my-20 anna-fade-in bg-gray-200 bg-opacity-75'>
+        <div className='flex flex-col items-center justify-center w-full'>
+
+          <div className='w-full flex items-center justify-center'>
+            <img src={data?.me.avatar} className={styles.avatar} />
+            <div className={styles.info}>
+              <h3 className='text-2xl'>{data?.me.name}</h3>
+              <h5 className='text-lg text-gray-800'>{t('app.profile.collected')} {data?.me.clippingsCount} {t('app.profile.records')}</h5>
+              {!data?.me.wechatOpenid && (
+                <WechatBindButton />
+              )}
+            </div>
+          </div>
+
+          <div className='w-full h-64 mt-6'>
+            <ResponsiveContainer>
+              <BarChart data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="count" fill="#0277d7" />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
         </div>
+
       </Card>
 
       <Divider title={t('app.profile.recents')} />
