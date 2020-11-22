76e665fab05eda44d234b02c1ae235be437f93a0
AnnatarHe <iamhele1994@gmail.com>
2020-11-22 17:35:54 +0800

    feat(help): update help video

--- src/pages/uploader/help.tsx
+++ src/pages/uploader/help.tsx
@@ -2,6 +2,21 @@ import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Dialog from '../../components/dialog/dialog'
 
+function BilibiliUploadHelpVideo() {
+  const { t } = useTranslation()
+  return (
+    <div className='w-full flex items-center justify-center my-8'>
+      <a
+        className='text-center text-gray-900 text-lg hover:text-red-300 dark:text-gray-300'
+        href='https://www.bilibili.com/video/BV11z4y1y7fx'
+        target='_blank'
+      >
+        {t('app.upload.help.title')}
+      </a>
+    </div>
+  )
+}
+
 function ClippingsUploadHelp() {
   const [visible, setVisible] = useState(false)
   const { t } = useTranslation()
@@ -34,4 +49,4 @@ function ClippingsUploadHelp() {
 }
 
 
-export default ClippingsUploadHelp
+export default BilibiliUploadHelpVideo
