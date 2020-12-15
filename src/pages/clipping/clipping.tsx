c8a4ff84c048fd862dec8c472db113e314de8ef4
AnnatarHe <iamhele1994@gmail.com>
2020-12-16 00:37:32 +0800

    fix(clipping): update clipping detail leading loose

--- src/pages/clipping/clipping.tsx
+++ src/pages/clipping/clipping.tsx
@@ -72,7 +72,7 @@ function ClippingPage(props: TClippingPageProp) {
           <h1 className='lg:text-3xl text-xl font-bold my-2'>{clipping?.clipping.title}</h1>
           <h3 className='font-light lg:text-lg my-4'>{book?.author}</h3>
           <hr className='bg-gray-400 my-12' />
-          <p className='lg:text-4xl text-2xl leading-normal'>{clippingContent}</p>
+          <p className='lg:text-3xl text-2xl lg:leading-loose leading-normal'>{clippingContent}</p>
           <hr className='bg-gray-400 my-12' />
           <time className='lg:text-base text-sm font-light text-right w-full block mt-4 text-gray-700'>
             {t('app.clipping.at')}: {clippingAt}
