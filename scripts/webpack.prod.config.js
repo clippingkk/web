36f87fb7bcbb677e65eb56dcc4b0e2ce1039b89d
AnnatarHe <iamhele1994@gmail.com>
2021-04-12 00:18:00 +0800

    feat(pwa): try to add pwa resource

--- scripts/webpack.prod.config.js
+++ scripts/webpack.prod.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 config.output.publicPath = '/'
 
@@ -14,13 +16,20 @@ config.plugins.push(
   new BundleAnalyzerPlugin({
     analyzerMode: 'static'
   }),
+  new WorkboxPlugin.GenerateSW({
+    clientsClaim: true,
+    skipWaiting: true
+  }),
+  new CopyWebpackPlugin([
+    { from: 'src/manifest', to: 'manifest' }
+  ])
 )
 config.optimization = {
   ...config.optimization,
   minimize: true,
   minimizer: [
     // new ESBuildMinifyPlugin({
-      // target: 'es2015' // Syntax to compile to (see options below for possible values)
+    // target: 'es2015' // Syntax to compile to (see options below for possible values)
     // }),
     ...config.optimization.minimizer,
     new TerserPlugin(),
--- src/App.tsx
+++ src/App.tsx
@@ -34,3 +34,14 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#app')
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration)
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      });
+  });
+}
