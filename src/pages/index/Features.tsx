f3f8089de6c60512097ec20130bd2803374fd84b
AnnatarHe <annatar.he+github@gmail.com>
2021-10-17 18:19:20 +0800

    feat(index): update index page

--- src/pages/index/Features.tsx
+++ src/pages/index/Features.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 type featureColorPattern = {
   title: string[]
@@ -57,6 +58,7 @@ type FeatureSectionType = {
   title: string
   items: {
     feature: string
+    link?: string
     desc?: string
     block?: React.ReactElement
   }[]
@@ -65,18 +67,29 @@ type FeatureSectionType = {
 
 function FeatureSection(props: FeatureSectionType) {
   return (
-    <div className='flex items-center w-full justify-around py-16'>
-      <h3 className={'text-8xl overflow-visible text-center max-w-xs ' + props.colorPattern.title.join(' ')}>
+    <div className='flex items-center w-full justify-around py-16 lg:flex-row flex-col'>
+      <h3 className={'lg:text-8xl text-4xl overflow-visible text-center max-w-xs mb-8 lg:mb-0 ' + props.colorPattern.title.join(' ')}>
         {props.title}
       </h3>
-      <ul className='text-6xl ml-12'>
+      <ul className='text-3xl lg:text-6xl lg:ml-12 ml-6'>
         {props.items.map(((x, i) => (
           <li key={x.feature} className='mb-10'>
-            <h4 className={props.colorPattern.rows[i]}>
-              {x.feature}
-            </h4>
+            {x.link ? (
+              <a
+                href={x.link}
+                target='_blank'
+                className={props.colorPattern.rows[i] + ' hover:underline'}
+                rel="noreferrer"
+              >
+                {x.feature}
+              </a>
+            ) : (
+              <h4 className={props.colorPattern.rows[i]}>
+                {x.feature}
+              </h4>
+            )}
             {x.desc && (
-              <h6 className='text-base'>
+              <h6 className={props.colorPattern.rows[i] + ' text-base mt-4'}>
                 {x.desc}
               </h6>
             )}
@@ -89,55 +102,70 @@ function FeatureSection(props: FeatureSectionType) {
 }
 
 function Features() {
+  const { t } = useTranslation()
   return (
     <div className='w-full flex justify-center flex-col p-10'>
       <h2 className='text-3xl text-center dark:text-gray-100'>Feature List</h2>
       <FeatureSection
-        title='Open'
+        title={t('app.index.features.open.title')}
         colorPattern={colorPatterns[0]}
         items={[{
-          feature: 'Export To Notion'
+          feature: t('app.index.features.open.f1'),
+          link: 'https://www.bilibili.com/video/BV1Tg411G7gG'
         }, {
-          feature: 'Export To Flomo'
+          feature: t('app.index.features.open.f2'),
+          link: 'https://annatarhe.notion.site/flomo-170afd99f866403d922a363b5b1b7706',
+          desc: t('app.index.features.open.f2Desc'),
         }, {
-          feature: 'Webhook'
+          feature: t('app.index.features.open.f3'),
+          link: 'https://annatarhe.notion.site/Webhook-24f26f59c0764365b3deb8e4c8e770ae'
         }, {
-          feature: 'Web widget',
-          desc: '支持在任意外部网站引用你的读书笔记 (除非微信屏蔽)',
-          block: (<p>preview image here</p>)
+          feature: t('app.index.features.open.f4'),
+          link: 'https://web-widget-script.pages.dev/',
+          desc: t('app.index.features.open.f4Desc'),
         }]}
       />
       <FeatureSection
         colorPattern={colorPatterns[1]}
-        title='Modern'
+        title={t('app.index.features.modern.title')}
         items={[{
-          feature: 'Arch'
+          feature: t('app.index.features.modern.f1'),
+          desc: t('app.index.features.modern.f1Desc'),
         }, {
-          feature: 'Code'
+          feature: t('app.index.features.modern.f2'),
+          desc: t('app.index.features.modern.f2Desc'),
         }, {
-          feature: 'Mind: the most important thing'
+          feature: t('app.index.features.modern.f3'),
+          desc: t('app.index.features.modern.f3Desc'),
         }]}
       />
       <FeatureSection
         colorPattern={colorPatterns[2]}
-        title='Sense'
+        title={t('app.index.features.sense.title')}
         items={[{
-          feature: 'iOS widget'
+          feature: t('app.index.features.sense.f1'),
+          link: 'https://apps.apple.com/us/app/clippingkk/id1537830952',
+          desc: t('app.index.features.sense.f1Desc'),
         }, {
-          feature: 'Privacy'
+          feature: t('app.index.features.sense.f2'),
+          desc: t('app.index.features.sense.f2Desc'),
         }, {
-          feature: 'Comminicate'
+          feature: t('app.index.features.sense.f3'),
+          desc: t('app.index.features.sense.f3Desc'),
         }]}
       />
       <FeatureSection
         colorPattern={colorPatterns[0]}
-        title='Make Reading Greate Aagain'
+        title={t('app.index.features.reading4.title')}
         items={[{
-          feature: 'platforms'
+          feature: t('app.index.features.reading4.f1'),
+          desc: t('app.index.features.reading4.f1Desc'),
         }, {
-          feature: 'close to user'
+          feature: t('app.index.features.reading4.f2'),
+          desc: t('app.index.features.reading4.f2Desc'),
         }, {
-          feature: 'free'
+          feature: t('app.index.features.reading4.f3'),
+          desc: t('app.index.features.reading4.f3Desc'),
         }]}
       />
     </div>
