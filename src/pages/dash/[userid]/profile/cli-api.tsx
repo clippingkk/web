2015f63676e1422f9f4750fbeb89ff4b2826d3f9
AnnatarHe <annatar.he+github@gmail.com>
2021-12-21 00:38:51 +0800

    feat(profile): add claim api token method

---
+++ src/pages/dash/[userid]/profile/cli-api.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type CliApiTokenProps = {
+}
+
+function CliApiToken(props: CliApiTokenProps) {
+  // TODO: request publick api token
+  return (
+    <div>CliApiToken</div>
+  )
+}
+
+export default CliApiToken
