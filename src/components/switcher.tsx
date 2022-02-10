b76706b7af225de6fef25a30fd418bd00584030c
AnnatarHe <annatar.he+github@gmail.com>
2022-02-10 23:13:51 +0800

    fix(switcher): fix switcher styles

--- src/components/switcher.tsx
+++ src/components/switcher.tsx
@@ -18,12 +18,12 @@ function Switcher ({
   return (
     <label
       {...props}
-      className={'relative inline-block w-6 h-6 align-middle cursor-pointer select-none ' + disabled ? 'opacity-70 cursor-not-allowed' : '' }
+      className={'relative inline-block w-6 h-6 align-middle cursor-pointer select-none ' + (disabled ? 'opacity-70 cursor-not-allowed' : '') }
       onClick={disabled ? undefined : () => onChange(!!checked)}
     >
       <input type="hidden" name={name} value={checked ? 1 : 0} />
-      <span className={'absolute top-0 left-0 bottom-0 right-0 rounded-lg bg-gray-200 ' + checked ? 'bg-purple-400' : '' } />
-      <span className={'absolute top-1 bottom-1 right-3 left-1 bg-white rounded-lg transition-all duration-100 ' + checked ? ' right-1 left-3 ' : ''} />
+      <span className={'absolute top-0 left-0 bottom-0 right-0 rounded-lg bg-gray-200 ' + (checked ? 'bg-purple-400' : '') } />
+      <span className={'absolute top-1 bottom-1 right-3 left-1 bg-white rounded-lg transition-all duration-100 ' + (checked ? ' right-1 left-3 ' : '')} />
     </label>
   )
 }
