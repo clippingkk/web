abdca0989c6cc1e893db8aa0638ba358e5809818
AnnatarHe <annatar.he+github@gmail.com>
2022-05-25 11:02:21 +0800

    fix(auth): remove walletconnect

--- src/components/auth.metamask.tsx
+++ src/components/auth.metamask.tsx
@@ -1,7 +1,7 @@
 import { useLazyQuery } from '@apollo/client'
 import React, { useCallback, useEffect, useRef } from 'react'
 import { toast } from 'react-toastify'
-import WalletConnectProvider from "@walletconnect/web3-provider"
+// import WalletConnectProvider from "@walletconnect/web3-provider"
 import { useAuthBy3rdPartSuccessed } from '../hooks/hooks'
 import { authByWeb3, authByWeb3Variables } from '../schema/__generated__/authByWeb3'
 import { metaMask, hooks, signDataByWeb3 } from '../utils/wallet'
@@ -10,83 +10,83 @@ import MetamaskLogo from './icons/metamask.logo.svg'
 import { useRouter } from 'next/router'
 import LoadingIcon from './icons/loading.svg'
 import WithLoading from './with-loading'
-import WalletConnect from '@walletconnect/client'
-import QRCodeModal from '@walletconnect/qrcode-modal'
+// import WalletConnect from '@walletconnect/client'
+// import QRCodeModal from '@walletconnect/qrcode-modal'
 import { isMobile } from '../utils/device'
 
 type AuthByMetamaskProps = {
 }
 
-function useWalletConnect() {
-  const wc = useRef<WalletConnectProvider | null>()
+// function useWalletConnect() {
+//   const wc = useRef<WalletConnectProvider | null>()
 
-  useEffect(() => {
-    // const connector = new WalletConnect({
-    //   bridge: "https://bridge.walletconnect.org", // Required
-    //   qrcodeModal: QRCodeModal,
-    // })
+//   useEffect(() => {
+//     // const connector = new WalletConnect({
+//     //   bridge: "https://bridge.walletconnect.org", // Required
+//     //   qrcodeModal: QRCodeModal,
+//     // })
 
-    //  Create WalletConnect Provider
-    const connector = new WalletConnectProvider({
-      infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
-      qrcodeModalOptions: {
-        mobileLinks: [
-          "rainbow",
-          "metamask",
-          "argent",
-          "trust",
-          "imtoken",
-          "pillar",
-        ],
-      },
-    });
+//     //  Create WalletConnect Provider
+//     const connector = new WalletConnectProvider({
+//       infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+//       qrcodeModalOptions: {
+//         mobileLinks: [
+//           "rainbow",
+//           "metamask",
+//           "argent",
+//           "trust",
+//           "imtoken",
+//           "pillar",
+//         ],
+//       },
+//     });
 
-    //  Enable session (triggers QR Code modal)
-    wc.current = connector
-    // connector.on("connect", (error, payload) => {
-    //   if (error) {
-    //     throw error;
-    //   }
+//     //  Enable session (triggers QR Code modal)
+//     wc.current = connector
+//     // connector.on("connect", (error, payload) => {
+//     //   if (error) {
+//     //     throw error;
+//     //   }
 
-    //   // Get provided accounts and chainId
-    //   const { accounts, chainId } = payload.params[0]
-    //   console.log('on connected', accounts, chainId)
-    // });
+//     //   // Get provided accounts and chainId
+//     //   const { accounts, chainId } = payload.params[0]
+//     //   console.log('on connected', accounts, chainId)
+//     // });
 
-    // connector.on("session_update", (error, payload) => {
-    //   if (error) {
-    //     throw error;
-    //   }
+//     // connector.on("session_update", (error, payload) => {
+//     //   if (error) {
+//     //     throw error;
+//     //   }
 
-    //   // Get updated accounts and chainId
-    //   const { accounts, chainId } = payload.params[0];
-    // });
+//     //   // Get updated accounts and chainId
+//     //   const { accounts, chainId } = payload.params[0];
+//     // });
 
-    // connector.on("disconnect", (error, payload) => {
-    //   if (error) {
-    //     throw error;
-    //   }
-    //   // Delete connector
-    // })
-  }, [])
+//     // connector.on("disconnect", (error, payload) => {
+//     //   if (error) {
+//     //     throw error;
+//     //   }
+//     //   // Delete connector
+//     // })
+//   }, [])
 
-  const onWeb3Login = useCallback(async () => {
-    const connector = wc.current
-    if (!connector) {
-      return
-    }
-    const addrs = await connector.enable();
-    console.log('on login response', addrs)
-    // if (!connector.connected) {
-    //   connector.createSession()
-    // }
-  }, [])
+//   const onWeb3Login = useCallback(async () => {
+//     const connector = wc.current
+//     if (!connector) {
+//       return
+//     }
+//     const addrs = await connector.enable();
+//     console.log('on login response', addrs)
+//     // if (!connector.connected) {
+//     //   connector.createSession()
+//     // }
+//   }, [])
 
-  return {
-    wc,
-    onWeb3Login
-  }
-}
+//   return {
+//     wc,
+//     onWeb3Login
+//   }
+// }
 
 function AuthByMetamask(props: AuthByMetamaskProps) {
   const router = useRouter()
