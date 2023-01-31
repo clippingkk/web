b6585cfcd40cebf3ef4209faf90dea29ffdc34fb
AnnatarHe <annatar.he+github@gmail.com>
2023-02-01 02:30:06 +0800

    perf(seo): update seo desc

--- src/components/og/og-with-index.tsx
+++ src/components/og/og-with-index.tsx
@@ -10,6 +10,7 @@ function OGWithIndex(props: OGWithIndexProps) {
   const url = APP_URL_ORIGIN
 
   const metaTitle = `Clippingkk - kindle 书摘管理`
+  const desc = 'ClippingKK 帮助用户整理 kindle 中的笔记到可控制，可展示的界面中，并且使得这些笔记可以在用户手机中展示，时常可以温故知新，时常想想之前的话语'
 
   const logoLink = APP_URL_ORIGIN + logo.src
 
@@ -19,19 +20,19 @@ function OGWithIndex(props: OGWithIndexProps) {
       <meta property="og:type" content='website' />
       <meta property="og:title" content={metaTitle} />
       <meta property="og:image" content={logoLink} />
-      <meta property="og:description" content={metaTitle} />
+      <meta property="og:description" content={desc} />
       <meta property="og:site_name" content='clippingkk' />
       <meta property="article:author" content='AnntarHe' />
 
-      <meta name='description' content={metaTitle} />
-      <meta name='keyword' content={`clippingkk, 书摘, 首页` } />
+      <meta name='description' content={desc} />
+      <meta name='keyword' content={`clippingkk, 书摘, 首页, kindle` } />
       <MetaTwitterCard
         card={TwitterCardType.summary}
         site='AnnatarHe'
         creator='AnnatarHe'
         url={url}
         title={metaTitle}
-        description={metaTitle}
+        description={desc}
         image={logoLink}
       />
     </React.Fragment>
