1517e26d69107b8733a4ef19ac7eb70f475bda8f
AnnatarHe <annatar.he+github@gmail.com>
2023-03-16 01:16:41 +0800

    feat(book): use blurhash to show placeholder of book image

--- src/components/book-cover/book-cover.tsx
+++ src/components/book-cover/book-cover.tsx
@@ -4,6 +4,7 @@ import styles from './book-cover.module.css'
 import HideUntilLoaded from '../SimpleAnimation/HideUntilLoaded';
 import Link from 'next/link'
 import Image from 'next/image'
+import BlurhashView from '@annatarhe/blurhash-react';
 
 type TBookCoverProps = {
   book: WenquBook
@@ -19,25 +20,14 @@ function BookCover({ book, domain }: TBookCoverProps) {
     <Link
       href={`/dash/${domain}/book/${book.doubanId}`}
       className={styles.cover + ' bg-transparent flex flex-col items-center content-center transition-all duration-300 rounded hover:bg-opacity-75 hover:bg-gray-300 hover:shadow-lg animate__fadeInDown w-128 h-156 overflow-visible font-lxgw'}>
-
-      <HideUntilLoaded
-        imageToLoad={book.image}
-      >
-        {/* <Image
-          src={book.image}
-          className={styles.image + ' rounded shadow-lg duration-300 transition-transform overflow-visible'}
-          width={300}
-          height={350}
-          alt={book.title}
-        /> */}
-        <img
-          src={book.image}
-          className={styles.image + ' rounded shadow-lg duration-300 transition-transform w-72 h-96 overflow-visible'}
-          // width={300}
-          // height={350}
-          alt={book.title}
-        />
-      </HideUntilLoaded>
+      <BlurhashView
+        blurhashValue={book.edges?.imageInfo.blurHashValue ?? 'LEHV6nWB2yk8pyo0adR*.7kCMdnj'}
+        src={book.image}
+        height={384}
+        width={320}
+        className={styles.image + ' rounded shadow-lg duration-300 transition-transform w-72 h-96 overflow-visible'}
+        alt={book.title}
+      />
       <div className='flex-col flex content-center items-center mt-4'>
         <h3 className='m-0 dark:text-gray-200'>{book.title}</h3>
         <h5 className='my-4 mx-0 dark:text-gray-500'>{book.author}</h5>
