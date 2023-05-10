fc909d43e95579a0f72fda00e714ec5a18fcc1cc
AnnatarHe <annatar.he+github@gmail.com>
2023-05-10 23:12:10 +0800

    feat(i18n): support dynamic load for locale languages

--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -2,54 +2,13 @@ import i18n from 'i18next'
 import languageDetector from 'i18next-browser-languagedetector'
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next/initReactI18next'
-import zhCN from '../locales/zhCN.json'
-import ko from '../locales/ko.json'
-import en from '../locales/en.json'
-
-function initI18NextInstance() {
-  const inst = i18n.createInstance()
-  inst
-    .use(languageDetector)
-    .use(initReactI18next)
-    // .use(resourcesToBackend((language: string, namespace: string) => import(`../locales/${language}.json`)))
-    .init({
-      resources: {
-        en: {
-          translation: en
-        },
-        zhCN: {
-          translation: zhCN
-        },
-        ko: {
-          translation: ko
-        }
-      },
-      react: {
-        useSuspense: false
-      },
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false
-      }
-    })
-  return inst
-}
 
 i18n.use(languageDetector)
   .use(initReactI18next)
-  // .use(resourcesToBackend((language: string, namespace: string) => import(`../locales/${language}.json`)))
+  .use(resourcesToBackend(
+    (language: string, namespace: string) => import(`../locales/${language}.json`))
+  )
   .init({
-    resources: {
-      en: {
-        translation: en
-      },
-      zhCN: {
-        translation: zhCN
-      },
-      ko: {
-        translation: ko
-      }
-    },
     react: {
       useSuspense: false
     },
