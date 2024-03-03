9e06ba442b33f99d6dea1707514eaf70d520f5b9
AnnatarHe <annatar.he+github@gmail.com>
2024-03-03 13:06:28 +0800

    fix(tracking): disable tracking and update ci actions to latest version

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,7 @@
 export async function register() {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./instrumentation.node')
   }
