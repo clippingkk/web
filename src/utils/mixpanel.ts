7f892cbaa2947420c9cdeea9617381537d3d6996
AnnatarHe <annatar.he+github@gmail.com>
2024-05-11 20:03:53 +0800

    fix(mixpanel): respect real env when setting mixpanel debug variable

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -1,7 +1,7 @@
 import mixpanel from 'mixpanel-browser'
 
-const IS_PROD = process.env.NODE_ENV === 'production'
+const IS_DEV = process.env.DEV === 'true'
 
 mixpanel.init("a117786751946ffe4a094d0b19a75a4a", {
-  debug: !!IS_PROD,
+  debug: IS_DEV,
 })
