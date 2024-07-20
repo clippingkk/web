1a585354c466a9821fb227452c1e3765b4ee6833
AnnatarHe <annatar.he+github@gmail.com>
2024-07-20 13:09:34 +0800

    feat(md): add readonly markdown preview component

---
+++ src/components/RichTextEditor/MarkdownPreview.tsx
@@ -0,0 +1,28 @@
+import { useRef, useEffect } from 'react'
+import CKLexicalBaseEditor from './index'
+import { LexicalEditor } from 'lexical'
+import { $convertFromMarkdownString, TRANSFORMERS } from '@lexical/markdown'
+
+type MarkdownPreviewProps = {
+  md: string
+}
+
+function MarkdownPreview(props: MarkdownPreviewProps) {
+  const { md } = props
+  const editor = useRef<LexicalEditor>(null)
+  useEffect(() => {
+    editor.current?.update(() => {
+      $convertFromMarkdownString(md, TRANSFORMERS)
+    })
+  }, [md])
+  return (
+    <CKLexicalBaseEditor
+      editable={false}
+      className='w-full px-2 focus:shadow focus:bg-slate-300 focus:outline-none rounded transition-all duration-150'
+      markdown={md}
+      ref={editor}
+    />
+  )
+}
+
+export default MarkdownPreview
