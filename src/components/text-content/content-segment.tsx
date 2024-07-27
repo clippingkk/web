11cad2268e45f1d4327ddd5f5755d113b60e5fdf
AnnatarHe <annatar.he+github@gmail.com>
2024-07-27 23:40:24 +0800

    fix(ci): ignore some wrong codes

--- src/components/text-content/content-segment.tsx
+++ src/components/text-content/content-segment.tsx
@@ -37,54 +37,54 @@ function ContentSegment(props: ContentSegmentProps) {
 
   return <span>{segment}</span>
 
-  if (symbolList.has(segment)) {
-    return <span>{segment}</span>
-  }
+  // if (symbolList.has(segment)) {
+  //   return <span>{segment}</span>
+  // }
 
 
-  if (creatable && !noun) {
-    return (
-      <Popover
-        withArrow
-        transitionProps={{ transition: 'pop', duration: 125 }}
-        width={300}
-      >
-        <Popover.Target>
-          <span>{segment}</span>
-        </Popover.Target>
-        <Popover.Dropdown>
-          <p className='w-full text-center text-xl'>{segment}</p>
-          <p>{t('app.nouns.empty.tip')}</p>
-          <div className='mt-4 flex justify-end'>
-            <Button onClick={() => onNounAdd(segment)}>{t('app.nouns.empty.add')}</Button>
-          </div>
-        </Popover.Dropdown>
-      </Popover>
-    )
-  }
+  // if (creatable && !noun) {
+  //   return (
+  //     <Popover
+  //       withArrow
+  //       transitionProps={{ transition: 'pop', duration: 125 }}
+  //       width={300}
+  //     >
+  //       <Popover.Target>
+  //         <span>{segment}</span>
+  //       </Popover.Target>
+  //       <Popover.Dropdown>
+  //         <p className='w-full text-center text-xl'>{segment}</p>
+  //         <p>{t('app.nouns.empty.tip')}</p>
+  //         <div className='mt-4 flex justify-end'>
+  //           <Button onClick={() => onNounAdd(segment)}>{t('app.nouns.empty.add')}</Button>
+  //         </div>
+  //       </Popover.Dropdown>
+  //     </Popover>
+  //   )
+  // }
 
-  // 不能创建，而且也没有内容，暂时不知道咋操作，先不做
-  if (!noun) {
-    return <span>{segment}</span>
-  }
+  // // 不能创建，而且也没有内容，暂时不知道咋操作，先不做
+  // if (!noun) {
+  //   return <span>{segment}</span>
+  // }
 
-  return (
-    <HoverCard
-      transitionProps={{ transition: 'pop', duration: 125 }}
-      arrowSize={14}
-      withArrow
-    >
-      <HoverCard.Target>
-        <span>{segment}</span>
-      </HoverCard.Target>
-      <HoverCard.Dropdown>
-        <NounContentRender
-          noun={noun}
-          updatable={updatable}
-          onUpdate={onNounUpdate} />
-      </HoverCard.Dropdown>
-    </HoverCard>
-  )
+  // return (
+  //   <HoverCard
+  //     transitionProps={{ transition: 'pop', duration: 125 }}
+  //     arrowSize={14}
+  //     withArrow
+  //   >
+  //     <HoverCard.Target>
+  //       <span>{segment}</span>
+  //     </HoverCard.Target>
+  //     <HoverCard.Dropdown>
+  //       <NounContentRender
+  //         noun={noun}
+  //         updatable={updatable}
+  //         onUpdate={onNounUpdate} />
+  //     </HoverCard.Dropdown>
+  //   </HoverCard>
+  // )
 }
 
 export default ContentSegment
