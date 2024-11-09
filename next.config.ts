a44c33532dafa41ee5379fb1846900a6656772de
AnnatarHe <annatar.he+github@gmail.com>
2024-11-09 14:17:22 +0000

    fix: ignore cache connection in build step

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const config: NextConfig = {
   generateBuildId: () => {
     return process.env.GIT_COMMIT ?? ''
   },
-  cacheHandler: isProd ? require.resolve('./cache-handler.mjs') : undefined,
+  cacheHandler: (isProd && process.env.CACHE_REDIS_URI) ? require.resolve('./cache-handler.mjs') : undefined,
   cacheMaxMemorySize: 0,
   experimental: {
     // ppr: true,
