ed4d16dcf3952e6918b2a56898a806d8ca40df13
AnnatarHe <annatar.he+github@gmail.com>
2025-04-19 18:02:07 +0800

    fix(orders): update order page

--- src/app/dash/[userid]/settings/orders/page.tsx
+++ src/app/dash/[userid]/settings/orders/page.tsx
@@ -93,7 +93,7 @@ async function OrdersTable(props: Props) {
                       <span 
                         className={`inline-flex items-center px-2.5 py-1 rounded-full text-xs font-medium ${o.status === SubscriptionStatus.Active ? 'bg-teal-100 text-teal-800 dark:bg-teal-900/50 dark:text-teal-300' : 
                           o.status === SubscriptionStatus.Pending ? 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/50 dark:text-yellow-300' : 
-                          'bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300'}`}
+                            'bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-300'}`}
                       >
                         {o.status}
                       </span>
